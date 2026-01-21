79cddfa825148a72552846f96084bd42aa6580df
Christian Jay Reyes <christianjayreyes6@gmail.com>
2026-01-21 23:52:54 +0800

    Updated path

--- my-react-app/src/pages/Login.jsx
+++ my-react-app/src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Login({ onLogin }) {
     setLoading(true)
 
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch('http://hospital-records-backend.vercel.app/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
